a896fa4aa31af02fcd82e3ba91dc1d55937e4327
uservxiii <109089244+uservxiii@users.noreply.github.com>
2026-01-27 00:11:47 +0700

    Update proxy.php
    Signed-off-by: uservxiii <109089244+uservxiii@users.noreply.github.com>

--- proxy.php
+++ proxy.php
@@ -1,25 +1,27 @@
 <?php
-if(!isset($_GET['u'])) exit;
-
-$url = $_GET['u'];
-$ch = curl_init($url);
-curl_setopt_array($ch, [
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_USERAGENT => "ORCA-WebScan",
-  CURLOPT_TIMEOUT => 15
-]);
-
-$body = curl_exec($ch);
-$info = curl_getinfo($ch);
-$err  = curl_error($ch);
-curl_close($ch);
-
-if($body===false){
-  http_response_code(502);
-  exit;
+/* =========================
+   SIMPLE PHP PROXY ENGINE
+========================= */
+function http_get($url){
+  $ch = curl_init($url);
+  curl_setopt_array($ch,[
+    CURLOPT_RETURNTRANSFER=>true,
+    CURLOPT_FOLLOWLOCATION=>true,
+    CURLOPT_USERAGENT=>"ORCA-WebScan",
+    CURLOPT_TIMEOUT=>15,
+    CURLOPT_SSL_VERIFYPEER=>false
+  ]);
+  $body = curl_exec($ch);
+  $info = curl_getinfo($ch);
+  curl_close($ch);
+  return [$body,$info];
 }
 
-http_response_code($info['http_code']);
-header("Content-Type: text/plain");
-echo $body;
+if(isset($_GET['fetch'])){
+  [$b,$i]=http_get($_GET['fetch']);
+  http_response_code($i['http_code']??200);
+  header("Content-Type:text/plain");
+  echo $b;
+  exit;
+}
+?>
